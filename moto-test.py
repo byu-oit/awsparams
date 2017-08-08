114bb5b9ce6fb90ab5d7bd28dd2328eba8fefb09
Nathan Peterson <ndpete@gmail.com>
2017-08-08 15:46:19 -0600

    testing changes to moto

---
+++ moto-test.py
@@ -0,0 +1,30 @@
+import boto3
+import sure
+from moto import mock_ssm
+
+
+@mock_ssm
+def moto_test_describe_parameters():
+    client = boto3.client('ssm', region_name='us-east-1')
+
+    client.put_parameter(
+        Name='test',
+        Description='A test parameter',
+        Value='value',
+        Type='String')
+
+    response = client.describe_parameters()
+
+    len(response['Parameters']).should.equal(1)
+    response['Parameters'][0]['Name'].should.equal('test')
+    response['Parameters'][0]['Type'].should.equal('String')
+
+
+def main():
+    print('Starting test')
+    moto_test_describe_parameters()
+    print('finished')
+
+
+if __name__ == '__main__':
+    moto_test_describe_parameters()
