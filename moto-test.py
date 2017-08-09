082f287763308561397a110d5bb84dba3ff174ed
Nathan Peterson <ndpete@gmail.com>
2017-08-09 10:42:35 -0600

    testing with more explicit definition

--- moto-test.py
+++ moto-test.py
@@ -7,7 +7,7 @@ import os
 @mock_ec2
 @mock_ssm
 def moto_test_describe_parameters():
-    client = boto3.client('ssm')
+    client = boto3.client('ssm', aws_access_key_id="FakeKey", aws_secret_access_key='FakeSecretKey', aws_session_token='FakeSessionToken')
 
     client.put_parameter(
         Name='test',
