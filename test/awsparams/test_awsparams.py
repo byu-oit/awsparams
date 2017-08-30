82111cd0e1340418867fbc4b525a99d9c49fe90c
Nathan Peterson <ndpete@gmail.com>
2017-08-30 14:52:02 -0600

    removed version test

--- test/awsparams/test_awsparams.py
+++ test/awsparams/test_awsparams.py
@@ -18,10 +18,6 @@ def fake_param():
     ssm.delete_parameter(Name=param['Name'])
 
 
-def test_version():
-    assert awsparams.__VERSION__ == '0.9.8'
-
-
 @mock_ssm
 def test_connect_ssm():
     assert awsparams.connect_ssm()
